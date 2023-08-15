99bdffabf33d77637aa0fbaba9807ffcb0731271
harrisb002 <harrisbe@somoma.edu>
2023-08-15 04:21:26 -0700

    Added session for user login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,8 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const adminEmails = ["harrisbe@sonoma.edu"];
+
 export default NextAuth({
   providers: [
     GoogleProvider({
@@ -11,4 +13,13 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
 });
